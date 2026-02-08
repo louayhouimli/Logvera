8c2d851ba432e5ba7760f323a9bb74190019418c
Louay Houimli <louayyhouimli@gmail.com>
2026-02-08 18:28:38 +0100

    Fixed font path

--- frontend/src/app/(marketing)/layout.tsx
+++ frontend/src/app/(marketing)/layout.tsx
@@ -14,42 +14,42 @@ import { AuthProvider } from "@/providers/auth-provider";
 const dmSans = localFont({
   src: [
     {
-      path: "/fonts/dm-sans/DMSans-Regular.ttf",
+      path: "../../fonts/dm-sans/DMSans-Regular.ttf",
       weight: "400",
       style: "normal",
     },
     {
-      path: "/fonts/dm-sans/DMSans-Italic.ttf",
+      path: "../../fonts/dm-sans/DMSans-Italic.ttf",
       weight: "400",
       style: "italic",
     },
     {
-      path: "/fonts/dm-sans/DMSans-Medium.ttf",
+      path: "../../fonts/dm-sans/DMSans-Medium.ttf",
       weight: "500",
       style: "normal",
     },
     {
-      path: "/fonts/dm-sans/DMSans-MediumItalic.ttf",
+      path: "../../fonts/dm-sans/DMSans-MediumItalic.ttf",
       weight: "500",
       style: "italic",
     },
     {
-      path: "/fonts/dm-sans/DMSans-SemiBold.ttf",
+      path: "../../fonts/dm-sans/DMSans-SemiBold.ttf",
       weight: "600",
       style: "normal",
     },
     {
-      path: "/fonts/dm-sans/DMSans-SemiBoldItalic.ttf",
+      path: "../../fonts/dm-sans/DMSans-SemiBoldItalic.ttf",
       weight: "600",
       style: "italic",
     },
     {
-      path: "/fonts/dm-sans/DMSans-Bold.ttf",
+      path: "../../fonts/dm-sans/DMSans-Bold.ttf",
       weight: "700",
       style: "normal",
     },
     {
-      path: "/fonts/dm-sans/DMSans-BoldItalic.ttf",
+      path: "../../fonts/dm-sans/DMSans-BoldItalic.ttf",
       weight: "700",
       style: "italic",
     },
